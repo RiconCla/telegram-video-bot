b8d47df92eae02a3612ed503b2b43c53fec592bc
Artem <cspirogone@yandex.ru>
2026-04-07 14:43:29 +0300

    fix deploy

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-// require('dotenv').config({ path: './credit.env' });
-require('dotenv').config();;
+require('dotenv').config();
+const path = require('path');
 
 module.exports = {
     // Telegram
